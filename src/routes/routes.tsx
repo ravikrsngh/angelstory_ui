ce73107897a6461488bfc136afe2031d9c09339c
Ravi Kumar Singh <ravikrsngh.rks@gmail.com>
2023-12-11 11:22:30 +0530

    Moved back collection route to private

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -11,24 +11,25 @@ import { BasicPage } from "../components/ui/basic-page";
 import { DesignLoader } from "../pages/design";
 
 const RouteData = () => {
-
   return (
     <Routes>
       <Route path="/" element={<BasicPage />}>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/templates" element={<Templates />} />
-        <Route path="/collection/:collectionId" element={<Collection />} />
         <Route path="/" element={<PrivateRoutes />}>
           <Route path="/account-dashboard" element={<AccountDashboard />} />
           <Route path="/dashboard" element={<Dashboard />} />
-          
+          <Route path="/collection/:collectionId" element={<Collection />} />
           <Route path="/collection/assets" element={<CollectionAssets />} />
         </Route>
       </Route>
       <Route path="/">
         <Route path="/" element={<PrivateRoutes />}>
-          <Route path="/design/:collectionId/:projectId" element={<DesignLoader />} />
+          <Route
+            path="/design/:collectionId/:projectId"
+            element={<DesignLoader />}
+          />
         </Route>
       </Route>
     </Routes>
