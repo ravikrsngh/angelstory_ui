e89525a30f43caa973eb46562d74cafe2345fbd8
Ravi <ravikrsngh.rks@gmail.com>
2024-04-16 09:57:52 +0530

    changed url

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -8,14 +8,14 @@ const beforeErrorHook = async (error: HTTPError) => {
 };
 
 export const userClient = ky.extend({
-  prefixUrl: "http://13.40.192.25:8080/",
+  prefixUrl: "https://api-dev.myangeljourney.com/",
   hooks: {
     beforeError: [beforeErrorHook],
   },
 });
 
 export const userAuthClient = ky.extend({
-  prefixUrl: "http://13.40.192.25:8080/",
+  prefixUrl: "https://api-dev.myangeljourney.com/",
   hooks: {
     beforeRequest: [
       (request) => {
