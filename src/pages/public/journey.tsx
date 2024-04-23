0ae550aa642d29b094834f6f28b19d72638714c4
Ravi <ravikrsngh.rks@gmail.com>
2024-04-23 10:59:50 +0530

    collection and journey public view pages

---
+++ src/pages/public/journey.tsx
@@ -0,0 +1,453 @@
+import { Tab } from "@headlessui/react";
+import { useState } from "react";
+import { NewCard } from "../../components/ui/cards";
+import {
+  FilesViewer,
+  FilesViewerItemType,
+} from "../../components/ui/files-viewer";
+import { useGetJourneyDetails } from "../../hooks/journey/use-journey-details";
+import {
+  AssetResType,
+  AssetTypes,
+  EntityType,
+  MemoryType,
+  MemoryTypes,
+} from "../../types";
+import { cn } from "../../utils";
+
+export const JourneyPublicPage = ({ id }: { id: number }) => {
+  const { data, isLoading, isFetching, isError } = useGetJourneyDetails(
+    String(id)
+  );
+  const [viewFilesViewer, setViewFilesViewer] = useState(false);
+  const [activeId, setActiveId] = useState<number>(0);
+  const [viewerData, setViewerData] = useState<FilesViewerItemType[]>([]);
+
+  const openFileViewer = (obj: FilesViewerItemType) => {
+    console.log(obj);
+
+    setViewFilesViewer(true);
+    setActiveId(obj.id);
+  };
+
+  if (isLoading || isFetching) {
+    return <span>Loading...</span>;
+  }
+  if (isError) {
+    return <span>Something went wrong!!</span>;
+  }
+  return (
+    <div className="px-4 md:px-10 lg:px-16 py-8 flex flex-col gap-10">
+      <div
+        className="relative rounded-md overflow-hidden p-4 pt-10 pb-5 md:p-10 md:pt-[140px] lg:pt-[200px] text-primary-950 bg-center bg-cover"
+        style={{
+          backgroundColor: data?.bgColor ? data.bgColor : "#CEB8AF",
+          backgroundImage: data?.bgImage ? `url(${data.bgImage})` : "",
+        }}
+      >
+        <span className="text-xs md:text-sm">Journey</span>
+        <h4 className="text-3xl md:text-4xl lg:text-5xl font-medium">
+          {data?.name}
+        </h4>
+      </div>
+
+      <div className="lg:mt-10">
+        <h4 className="font-medium mb-5 md:mb-10 text-xl flex justify-between items-center">
+          Assets
+        </h4>
+        <div>
+          <Tab.Group>
+            <Tab.List>
+              <div className="flex gap-4 border-b border-slate-300 items-center mb-8">
+                <Tab>
+                  {({ selected }) => (
+                    <button
+                      className={cn(
+                        "px-3 md:px-10 py-3 font-medium",
+                        selected
+                          ? "text-primary-400 border-b-2 border-primary-400"
+                          : ""
+                      )}
+                    >
+                      All
+                    </button>
+                  )}
+                </Tab>
+                <Tab>
+                  {({ selected }) => (
+                    <button
+                      className={cn(
+                        "px-3 md:px-10 py-3 font-medium",
+                        selected
+                          ? "text-primary-400 border-b-2 border-primary-400"
+                          : ""
+                      )}
+                    >
+                      Images
+                    </button>
+                  )}
+                </Tab>
+                <Tab>
+                  {({ selected }) => (
+                    <button
+                      className={cn(
+                        "px-3 md:px-10 py-3 font-medium",
+                        selected
+                          ? "text-primary-400 border-b-2 border-primary-400"
+                          : ""
+                      )}
+                    >
+                      Audio
+                    </button>
+                  )}
+                </Tab>
+                <Tab>
+                  {({ selected }) => (
+                    <button
+                      className={cn(
+                        "px-3 md:px-10 py-3 font-medium",
+                        selected
+                          ? "text-primary-400 border-b-2 border-primary-400"
+                          : ""
+                      )}
+                    >
+                      Video
+                    </button>
+                  )}
+                </Tab>
+              </div>
+            </Tab.List>
+            <Tab.Panels>
+              <Tab.Panel>
+                <div className="flex gap-4 overflow-auto">
+                  {data.assetList?.map((asset: AssetResType) => (
+                    <NewCard
+                      key={asset.id}
+                      type={asset.assetType}
+                      name={asset.name}
+                      dropdownOptions={[]}
+                      dataObject={asset}
+                      onClickHandler={() => {
+                        setViewerData(
+                          data.assetList?.map((asset: AssetResType) => {
+                            return {
+                              type: asset.assetType,
+                              entityType: EntityType.ASSET,
+                              id: asset.id,
+                              name: asset.name,
+                              src: asset.assetUrl,
+                            };
+                          })
+                        );
+                        openFileViewer({
+                          type: asset.assetType,
+                          src: asset.assetUrl,
+                          name: asset.name,
+                          entityType: EntityType.ASSET,
+                          id: asset.id,
+                        });
+                      }}
+                    />
+                  ))}
+                </div>
+              </Tab.Panel>
+              <Tab.Panel>
+                <div className="flex gap-4 overflow-auto">
+                  {data.assetList
+                    ?.filter(
+                      (asset: AssetResType) =>
+                        asset.assetType == AssetTypes.IMAGE
+                    )
+                    .map((asset: AssetResType) => (
+                      <NewCard
+                        key={asset.id}
+                        type={asset.assetType}
+                        name={asset.name}
+                        dropdownOptions={[]}
+                        dataObject={asset}
+                      />
+                    ))}
+                </div>
+              </Tab.Panel>
+              <Tab.Panel>
+                <div className="flex gap-4 overflow-auto">
+                  {data.assetList
+                    ?.filter(
+                      (asset: AssetResType) =>
+                        asset.assetType == AssetTypes.AUDIO
+                    )
+                    .map((asset: AssetResType) => (
+                      <NewCard
+                        key={asset.id}
+                        type={asset.assetType}
+                        name={asset.name}
+                        dropdownOptions={[]}
+                        dataObject={asset}
+                      />
+                    ))}
+                </div>
+              </Tab.Panel>
+              <Tab.Panel>
+                <div className="flex gap-4 overflow-auto">
+                  {data.assetList
+                    ?.filter(
+                      (asset: AssetResType) =>
+                        asset.assetType == AssetTypes.VIDEO
+                    )
+                    .map((asset: AssetResType) => (
+                      <NewCard
+                        key={asset.id}
+                        type={asset.assetType}
+                        name={asset.name}
+                        dropdownOptions={[]}
+                        dataObject={asset}
+                      />
+                    ))}
+                </div>
+              </Tab.Panel>
+            </Tab.Panels>
+          </Tab.Group>
+        </div>
+        <div className="mt-10">
+          <h4 className="font-medium mb-10 text-xl flex justify-between items-center">
+            Memories
+          </h4>
+
+          <div>
+            <Tab.Group>
+              <Tab.List>
+                <div className="flex gap-4 border-b border-slate-300 items-center mb-8">
+                  <Tab>
+                    {({ selected }) => (
+                      <button
+                        className={cn(
+                          "px-3 md:px-10 py-3 font-medium",
+                          selected
+                            ? "text-primary-400 border-b-2 border-primary-400"
+                            : ""
+                        )}
+                      >
+                        All
+                      </button>
+                    )}
+                  </Tab>
+                  <Tab>
+                    {({ selected }) => (
+                      <button
+                        className={cn(
+                          "px-3 md:px-10 py-3 font-medium",
+                          selected
+                            ? "text-primary-400 border-b-2 border-primary-400"
+                            : ""
+                        )}
+                      >
+                        Images
+                      </button>
+                    )}
+                  </Tab>
+                  <Tab>
+                    {({ selected }) => (
+                      <button
+                        className={cn(
+                          "px-3 md:px-10 py-3 font-medium",
+                          selected
+                            ? "text-primary-400 border-b-2 border-primary-400"
+                            : ""
+                        )}
+                      >
+                        Audio
+                      </button>
+                    )}
+                  </Tab>
+                  <Tab>
+                    {({ selected }) => (
+                      <button
+                        className={cn(
+                          "px-3 md:px-10 py-3 font-medium",
+                          selected
+                            ? "text-primary-400 border-b-2 border-primary-400"
+                            : ""
+                        )}
+                      >
+                        Video
+                      </button>
+                    )}
+                  </Tab>
+                  <Tab>
+                    {({ selected }) => (
+                      <button
+                        className={cn(
+                          "px-3 md:px-10 py-3 font-medium",
+                          selected
+                            ? "text-primary-400 border-b-2 border-primary-400"
+                            : ""
+                        )}
+                      >
+                        Cards
+                      </button>
+                    )}
+                  </Tab>
+                  <Tab>
+                    {({ selected }) => (
+                      <button
+                        className={cn(
+                          "px-3 md:px-10 py-3 font-medium",
+                          selected
+                            ? "text-primary-400 border-b-2 border-primary-400"
+                            : ""
+                        )}
+                      >
+                        Slideshow
+                      </button>
+                    )}
+                  </Tab>
+                </div>
+              </Tab.List>
+              <Tab.Panels>
+                <div>
+                  <Tab.Panel className="flex gap-4 flex-wrap">
+                    {data?.projectList?.map((memory: MemoryType) => (
+                      <NewCard
+                        key={memory.id}
+                        type={memory.projectType}
+                        name={memory.name}
+                        dropdownOptions={[]}
+                        dataObject={memory}
+                        onClickHandler={() => {
+                          setViewerData(
+                            data.projectList?.map((mm: MemoryType) => {
+                              return {
+                                type: mm.projectType,
+                                entityType: EntityType.MEMORY,
+                                id: mm.id,
+                                name: mm.name,
+                                src: mm.previewImage,
+                              };
+                            })
+                          );
+                          openFileViewer({
+                            type: memory.projectType,
+                            entityType: EntityType.MEMORY,
+                            id: memory.id,
+                            name: memory.name,
+                            src: memory.previewImage,
+                          });
+                        }}
+                      />
+                    ))}
+                  </Tab.Panel>
+                  <Tab.Panel className="flex gap-4 flex-wrap">
+                    {data?.projectList
+                      ?.filter(
+                        (memory: MemoryType) =>
+                          memory.projectType == MemoryTypes.IMAGE
+                      )
+                      .map((memory: MemoryType) => (
+                        <NewCard
+                          key={memory.id}
+                          type={memory.projectType}
+                          name={memory.name}
+                          dropdownOptions={[]}
+                          dataObject={memory}
+                        />
+                      ))}
+                  </Tab.Panel>
+                  <Tab.Panel className="flex gap-4 flex-wrap">
+                    {data?.projectList
+                      ?.filter(
+                        (memory: MemoryType) =>
+                          memory.projectType == MemoryTypes.AUDIO
+                      )
+                      .map((memory: MemoryType) => (
+                        <NewCard
+                          key={memory.id}
+                          type={memory.projectType}
+                          name={memory.name}
+                          dropdownOptions={[]}
+                          dataObject={memory}
+                        />
+                      ))}
+                  </Tab.Panel>
+                  <Tab.Panel className="flex gap-4 flex-wrap">
+                    {data?.projectList
+                      ?.filter(
+                        (memory: MemoryType) =>
+                          memory.projectType == MemoryTypes.VIDEO
+                      )
+                      .map((memory: MemoryType) => (
+                        <NewCard
+                          key={memory.id}
+                          type={memory.projectType}
+                          name={memory.name}
+                          dropdownOptions={[]}
+                          dataObject={memory}
+                        />
+                      ))}
+                  </Tab.Panel>
+                  <Tab.Panel className="flex gap-4 flex-wrap">
+                    {data?.projectList
+                      ?.filter(
+                        (memory: MemoryType) =>
+                          memory.projectType == MemoryTypes.VIDEO
+                      )
+                      .map((memory: MemoryType) => (
+                        <NewCard
+                          key={memory.id}
+                          type={memory.projectType}
+                          name={memory.name}
+                          dropdownOptions={[]}
+                          dataObject={memory}
+                        />
+                      ))}
+                  </Tab.Panel>
+                  <Tab.Panel className="flex gap-4 flex-wrap">
+                    {data?.projectList
+                      ?.filter(
+                        (memory: MemoryType) =>
+                          memory.projectType == MemoryTypes.CARD
+                      )
+                      .map((memory: MemoryType) => (
+                        <NewCard
+                          key={memory.id}
+                          type={memory.projectType}
+                          name={memory.name}
+                          dropdownOptions={[]}
+                          dataObject={memory}
+                        />
+                      ))}
+                  </Tab.Panel>
+                  <Tab.Panel className="flex gap-4 flex-wrap">
+                    {data?.projectList
+                      ?.filter(
+                        (memory: MemoryType) =>
+                          memory.projectType == MemoryTypes.SLIDESHOW
+                      )
+                      .map((memory: MemoryType) => (
+                        <NewCard
+                          key={memory.id}
+                          type={memory.projectType}
+                          name={memory.name}
+                          dropdownOptions={[]}
+                          dataObject={memory}
+                        />
+                      ))}
+                  </Tab.Panel>
+                </div>
+              </Tab.Panels>
+            </Tab.Group>
+          </div>
+        </div>
+        {viewFilesViewer ? (
+          <FilesViewer
+            items={viewerData}
+            setView={setViewFilesViewer}
+            activeId={activeId}
+            collectionId={data?.collectionId}
+            journeyId={data?.id}
+            publicView={true}
+          />
+        ) : null}
+      </div>
+    </div>
+  );
+};
